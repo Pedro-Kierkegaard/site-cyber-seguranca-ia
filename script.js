8266794eae8ecdf7fefe79f387b584d96dbaff19
Pedro-Kierkegaard <pedro_vieira@msn.com>
2026-04-13 19:12:51 -0300

    Adicionando script JavaScript para o site

--- script.js
+++ script.js
@@ -1,2 +1,2 @@
-// Podemos adicionar funcionalidades interativas aqui.
-console.log('Bem-vindo ao site de Cyber Segurança e Inteligência Artificial!');
+// script.js
+console.log('Bem-vindo ao site sobre Cyber Segurança e Inteligência Artificial!');
